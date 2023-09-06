998fba60a0ce512636779b79d1c951ed16030e49
WRXer <wrxerrr@gmail.com>
2023-09-06 12:13:16 +0400

    подключил документацию swagger

--- skymarket/skymarket/urls.py
+++ skymarket/skymarket/urls.py
@@ -2,11 +2,30 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 
 # TODO здесь необходимо подклюючит нужные нам urls к проекту
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API Documentation",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.example.com/policies/terms/",
+        contact=openapi.Contact(email="contact@example.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path("api/admin/", admin.site.urls),
-    path("api/redoc-tasks/", include("redoc.urls")),
+
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
