322ab88eff6270abcb7b3105a1f971ab38b53fd8
WRXer <wrxerrr@gmail.com>
2023-09-15 18:09:36 +0400

    исправил урлы под фронт

--- skymarket/config/urls.py
+++ skymarket/config/urls.py
@@ -24,10 +24,10 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("users.urls", namespace='users')),
-    path("", include("ads.urls", namespace='ads')),
+    path("api/", include("users.urls", namespace='users')),
+    path("api/", include("ads.urls", namespace='ads')),
 
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
