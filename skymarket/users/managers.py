5a31b061f2afb0721174ba0baa3999dea591bc9c
svro <ichistrawberry2012@gmail.com>
2023-11-10 00:15:19 +0300

    user conf_1

--- skymarket/users/managers.py
+++ skymarket/users/managers.py
@@ -3,5 +3,43 @@ from django.contrib.auth.models import (
 )
 # TODO здесь должен быть менеджер для модели Юзера.
 # TODO Поищите эту информацию в рекомендациях к проекту
+
+
 class UserManager(BaseUserManager):
-    pass
+    """
+    Функция создания пользователя — в нее мы передаем обязательные поля
+    """
+
+    def create_user(self, email, first_name, last_name, phone, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            role="user"
+        )
+        user.is_active = True
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, first_name, last_name, phone, password=None):
+        """
+        Функция для создания суперпользователя — с ее помощью мы создаем админинстратора
+        это можно сделать с помощью команды createsuperuser
+        """
+
+        user = self.create_user(
+            email,
+            first_name=first_name,
+            last_name=last_name,
+            phone=phone,
+            password=password,
+            role="admin"
+        )
+
+        user.save(using=self._db)
+        return user
--- skymarket/users/models.py
+++ skymarket/users/models.py
@@ -1,16 +1,54 @@
 from django.contrib.auth.models import AbstractBaseUser, AbstractUser
 from django.db import models
-from users.managers import UserManager
+from skymarket.users.managers import UserManager
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 
+# Constants
+NULLABLE = {'blank': True, 'null': True}
 
-class UserRoles:
+
+class UserRoles(models.TextChoices):
     # TODO закончите enum-класс для пользователя
-    pass
+    USER = 'user'
+    ADMIN = 'admin'
 
 
 class User(AbstractBaseUser):
     # TODO переопределение пользователя.
     # TODO подробности также можно поискать в рекоммендациях к проекту
-    pass
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone', 'role']
+
+    first_name = models.CharField(max_length=100, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+    phone = PhoneNumberField(max_length=100, verbose_name='Номер телефона', **NULLABLE)
+    email = models.EmailField(unique=True, verbose_name='Электронная почта')
+    image = models.ImageField(upload_to='users/avatars', verbose_name='Аватар', **NULLABLE)
+    role = models.CharField(max_length=50, choices=UserRoles.choices, default=UserRoles.USER, verbose_name='Роль')
+    is_active = models.BooleanField(default=True)
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    @property
+    def is_admin(self):
+        return self.role == UserRoles.ADMIN
+
+    @property
+    def is_user(self):
+        return self.role == UserRoles.USER
+
+    @property
+    def is_superuser(self):
+        return self.is_admin
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
