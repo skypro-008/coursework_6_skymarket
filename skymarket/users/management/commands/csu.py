78700765e4c025b5fd80957828db27e59cccfe26
svro <ichistrawberry2012@gmail.com>
2023-11-14 18:05:23 +0300

    csu

---
+++ skymarket/users/management/commands/csu.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from users.models import User
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        # Admin
+        user = User.objects.create(
+            email='svetars2015@yandex.ru',
+            is_staff=True,
+            is_superuser=True,
+            is_active=True
+        )
+
+        user.set_password('123genshin456')
+        user.save()
