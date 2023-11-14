cfe7ad24ff825a7314b63b9dd193f8ea5251ad5c
svro <ichistrawberry2012@gmail.com>
2023-11-14 11:39:42 +0300

    ads serializer

--- skymarket/ads/serializers.py
+++ skymarket/ads/serializers.py
@@ -1,18 +1,32 @@
 from rest_framework import serializers
-
+from .models import Comment, Ad
 
 # TODO Сериалайзеры. Предлагаем Вам такую структуру, однако вы вправе использовать свою
 
+
 class CommentSerializer(serializers.ModelSerializer):
     # TODO сериалайзер для модели
-    pass
+    author_first_name = serializers.CharField(source="author.first_name", read_only=True)
+    author_last_name = serializers.CharField(source="author.last_name", read_only=True)
+
+    class Meta:
+        model = Comment
+        exclude = ('id', 'author')
 
 
 class AdSerializer(serializers.ModelSerializer):
     # TODO сериалайзер для модели
-    pass
+    class Meta:
+        model = Ad
+        fields = ('pk', 'title', 'image', 'price', 'description')
 
 
 class AdDetailSerializer(serializers.ModelSerializer):
     # TODO сериалайзер для модели
-    pass
+    author_first_name = serializers.CharField(source="author.first_name", read_only=True)
+    author_last_name = serializers.CharField(source="author.last_name", read_only=True)
+    phone = serializers.CharField(source='author.phone')
+
+    class Meta:
+        model = Ad
+        fields = '__all__'
