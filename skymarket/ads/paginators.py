8e92fc938a940741d1ac339c86298891e85090b8
svro <ichistrawberry2012@gmail.com>
2023-11-14 12:27:32 +0300

    ads paginators

---
+++ skymarket/ads/paginators.py
@@ -0,0 +1,13 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class AdPagination(PageNumberPagination):
+    '''Пагинация для списка объявлений'''
+    page_size = 4
+    page_size_query_param = 'page_size'
+
+
+class CommentPagination(PageNumberPagination):
+    '''Пагинация для отзывов'''
+    page_size = 4
+    page_size_query_param = 'page_size'
