2aa5a27bc1f40094a35e922700611e88f3f48602
svro <ichistrawberry2012@gmail.com>
2023-11-14 12:07:09 +0300

    ads permissions

--- skymarket/ads/permissions.py
+++ skymarket/ads/permissions.py
@@ -1,1 +1,23 @@
 # TODO здесь производится настройка пермишенов для нашего проекта
+from rest_framework.permissions import BasePermission
+from users.models import UserRoles
+
+
+class IsAdmin(BasePermission):
+    '''Доступ только для авторизированных пользователей с ролью ADMIN'''
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.role == UserRoles.ADMIN
+
+
+class IsOwner(BasePermission):
+    '''Достип авторизированного пользователя к своему объявлению'''
+
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user
