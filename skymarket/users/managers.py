731967e0127ca04e0df4c57dd9f17cad716b6035
svro <ichistrawberry2012@gmail.com>
2023-11-16 14:56:46 +0300

    red managers.py

--- skymarket/users/managers.py
+++ skymarket/users/managers.py
@@ -10,7 +10,7 @@ class UserManager(BaseUserManager):
     Функция создания пользователя — в нее мы передаем обязательные поля
     """
 
-    def create_user(self, email, first_name, last_name, phone, password=None):
+    def create_user(self, email, first_name, last_name, phone, password=None, role='user'):
         if not email:
             raise ValueError('Users must have an email address')
         user = self.model(
@@ -18,7 +18,7 @@ class UserManager(BaseUserManager):
             first_name=first_name,
             last_name=last_name,
             phone=phone,
-            role="user"
+            role=role
         )
         user.is_active = True
         user.set_password(password)
@@ -26,7 +26,7 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, first_name, last_name, phone, password=None):
+    def create_superuser(self, email, first_name, last_name, phone, password=None, role='admin'):
         """
         Функция для создания суперпользователя — с ее помощью мы создаем админинстратора
         это можно сделать с помощью команды createsuperuser
@@ -38,7 +38,7 @@ class UserManager(BaseUserManager):
             last_name=last_name,
             phone=phone,
             password=password,
-            role="admin"
+            role=role
         )
 
         user.save(using=self._db)
